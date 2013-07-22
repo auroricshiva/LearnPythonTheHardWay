5843b30b221454fef67441113f64cd4b5b1d4039
Ricky <auroricshiva@gmail.com>
2013-07-21 23:38:04 -0400

    Exercise 47 - Automated Testing

--- ex47/setup.py
+++ ex47/setup.py
@@ -5,10 +5,10 @@ except ImportError:
 
 config = {
     'description': 'Exercise 47 from Learn Python the Hard Way',
-    'author': 'R. Liang',
-    'url': 'http://rickyliang.com',
-    'download_url': 'http://rickyliang.com/downloads',
-    'author_email': 'rickyliang@rickyliang.com',
+    'author': 'auroricshiva',
+    'url': '',
+    'download_url': '',
+    'author_email': '',
     'version': '0.1',
     'install_requires': ['nose'],
     'packages': ['NAME'],
